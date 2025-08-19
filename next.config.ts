6b63923d48d79c5ae273ac0a7601c5d796ed3c42
Uchqun Turdiyev <uchqunjonturdiev@gmail.com>
2025-08-19 10:29:22 +0500

    change error clear

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Build paytida ESLint xatolarini to‘xtatmaydi
-    ignoreDuringBuilds: true,
-  },
 }
-
 export default nextConfig
